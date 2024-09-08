369785f8c52de2c7dd96e6d1fbd4f596256e0f1d
최용화 <yonghwa9086@naver.com>
2024-09-08 14:14:03 +0900

    3.4 카프카 클라이언트 - kafka-producer-custom-partitioner 예제 업로드

---
+++ chapter3.4.1-kafka-producer-custom-partitioner/src/main/java/com/madcoder/ProducerWithCustomPartitioner.java
@@ -0,0 +1,27 @@
+package com.madcoder;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerWithCustomPartitioner {
+    private static final String TOPIC_NAME = "test";
+    private static final String BOOTSTRAP_SERVERS = "madcoder-kafka-server:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
